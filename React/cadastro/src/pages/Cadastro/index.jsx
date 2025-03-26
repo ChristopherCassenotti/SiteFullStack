427533a5d5b658e1672f2a5b153187fe33e9acb8
ChristopherCassenotti <craftplays10a@gmail.com>
2025-03-26 20:37:18 -0300

    Incrementando FrontEnd

---
+++ React/cadastro/src/pages/Cadastro/index.jsx
@@ -0,0 +1,14 @@
+function Cadastro() {
+  return (
+    <div>
+      <h2>Cadastro</h2>
+      <form action="">
+        <input type="text" placeholder="Nome" />
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+      </form>
+    </div>
+  );
+}
+
+export default Cadastro;
---
+++ React/cadastro/src/pages/Login/index.jsx
@@ -0,0 +1,13 @@
+function Login() {
+  return (
+    <div>
+      <h2>Login</h2>
+      <form action="">
+        <input type="email" placeholder="Email" />
+        <input type="password" placeholder="Senha" />
+      </form>
+    </div>
+  );
+}
+
+export default Login;
